62dea8895a6eb3f558144cbc5f13256e4da12f4a
shivam <mailtoshivam2002@gmail.com>
2023-01-23 20:25:17 +0530

    Final Submission

--- src/Components/Userdetails.js
+++ src/Components/Userdetails.js
@@ -1,15 +1,54 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import classes from "./Userdetails.module.css";
+
+const UserDetails = () => {
+  const location = useLocation();
+  const prod = location.state;
+
+  console.log(prod);
+  return (
+    <section>
+      <h1>You are inside user UserDetails</h1>
+      <div className={classes.div1}>
+        <h1>Hello Travellers</h1>
+        <span className={classes.span1}>Contact---{prod.name}</span>
+        <span className={classes.span1}>city---{prod.city}</span>
+        <span className={classes.span1}>street---{prod.street}</span>
+        <Link to="/user"><button>Hide Detais</button></Link>
+        </div>
+        <div className={classes.div2}>
+            <h4>Description</h4>
+            <h5>Providing service of deomestic fligh booking @ lowest with this we are providing loyal customer service</h5>
+            <div>
+                <span className={classes.span2}>Contact Person</span>
+                <span className={classes.span2}>address</span>
+            </div>
+            <div>
+                <span className={classes.span2}>{prod.name}</span>
+                <span className={classes.span2}>{prod.city},{prod.street}</span>
+            </div>
+            <div>
+                <span className={classes.span2}>Email</span>
+                <span className={classes.span2}>city</span>
+            </div>
+            <div>
+                <span className={classes.span2}>{prod.email}</span>
+                <span className={classes.span2}>{prod.city}</span>
+            </div>
+            <div>
+                <span className={classes.span2}>Phone</span>
+                <span className={classes.span2}>Country</span>
+            </div>
+            <div>
+                <span className={classes.span2}>{prod.phone}</span>
+                <span className={classes.span2}>India</span>
+            </div>
 
 
-const UserDetails = (props) => {
-    
-    console.log(props.name);
-    return(
-        <section>
-            <h1>You are inside user UserDetails</h1>
-            
-        </section>
-    )
+        </div>
+    </section>
+  );
 };
 
 export default UserDetails;
--- src/Components/showuser.js
+++ src/Components/showuser.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import classes from './Showuser.module.css';
-import UserDetails from "./Userdetails";
-
+import classes from "./Showuser.module.css";
 
 const ShowUser = (props) => {
-    console.log("inside showuser")
-    return (
-        <>
-        
-        <div className={classes.div1} >
-            <span>{props.name}</span>
-            <span>{props.city}</span>
-            <span>{props.street}</span>
-            <Link to='/user/userdetails' UserDetails={props}>
-            <button>view more</button>
-            </Link>
-        </div>
-        
-        </>
-    )
+  console.log("inside showuser");
+  return (
+    <>
+      <div className={classes.div1}>
+        <span className={classes.span1}>{props.name}</span>
+        <span className={classes.span1}>{props.city}</span>
+        <span className={classes.span1}>{props.street}</span>
+        <Link to={{ pathname: "/user/userdetails", state: props }}>
+          <button>view more</button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default ShowUser;
